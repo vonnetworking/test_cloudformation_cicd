5bbef90341f67558a60dddb9a34af8d7a8a52c2c
Av2 <vonnetworking2@gmail.com>
2019-06-02 17:35:27 -0400

    adjusting decompose script

--- util/_jenkins_decompose_commit.sh
+++ util/_jenkins_decompose_commit.sh
@@ -14,19 +14,19 @@ for F in $CHANGED_YAMLS; do
   PKG_BASE=`echo $(basename $F | awk -F'.' '{print $1}')`
   echo "$PKG_BASE"
   pwd
-  find . -name ${PKG_BASE}* | zip -r -@ ./stage/${PKG_BASE}.zip
+  find .. -name ${PKG_BASE}* | zip -r -@ ./stage/${PKG_BASE}.zip
 done
 
 for F in $CHANGED_BOOTSTRAPS; do
   PKG_BASE=`echo $(basename $F | awk -F'.' '{print $1}')`
   echo "$PKG_BASE"
   pwd
-  find . -name ${PKG_BASE}* | zip -r -@ ./stage/${PKG_BASE}.zip
+  find .. -name ${PKG_BASE}* | zip -r -@ ./stage/${PKG_BASE}.zip
 done
 
 for F in $CHANGED_PARAMS; do
   PKG_BASE=`echo $(basename $F | awk -F'.' '{print $1}')`
   echo "$PKG_BASE"
   pwd
-  find . -name ${PKG_BASE}* | zip -r -@ ./stage/${PKG_BASE}.zip
+  find .. -name ${PKG_BASE}* | zip -r -@ ./stage/${PKG_BASE}.zip
 done
