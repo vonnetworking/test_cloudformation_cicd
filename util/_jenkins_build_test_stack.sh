f3c2e6e2cccf844fa02f65f03cbf3f05fe2891ec
Av2 <vonnetworking2@gmail.com>
2019-06-02 13:06:52 -0400

    again

--- util/_jenkins_build_test_stack.sh
+++ util/_jenkins_build_test_stack.sh
@@ -53,7 +53,7 @@ function build_stack () {
 
    /usr/local/bin/aws cloudformation create-stack \
   --stack-name="$STACKNAME" \
-  --template-body="s3://av-cloud-meta-data-us-east-1/stage/landing-zone/BasicGoodLandingZone.yaml"
+  --template-body="s3://av-cloud-meta-data-us-east-1/stage/landing-zone/BasicGoodLandingZone.yaml" \
   --parameters="s3://av-cloud-meta-data-us-east-1/stage/params/BasicGoodLandingZone.yaml" > ./build_stack.out
   #--template-body="${AWS_STAGE_CFT_S3_BUCKET}/${CLOUDFORMATION}" \
   #--parameters="${AWS_STAGE_CFT_S3_BUCKET}/${CLOUDFORMATION_TEST_PARAMS}" > './build_stack.out'
